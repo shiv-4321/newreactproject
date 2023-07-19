9747f5fd03958476c77fd7d4333f2521cf399399
shiv-4321 <spsaxena97@gmail.com>
2023-07-20 00:03:24 +0530

    UI updated

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+    return <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="inner-content">
+                        <p>Copyright &copy; 2020 Sixteen Clothing Co., Ltd.
+
+                            - Design: <a rel="nofollow noopener" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+}
+
+export default Footer
---
+++ src/pages/Rootlayout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const Rootlayout = () => {
+    return <>
+        <div id="preloader" >
+            <div class="jumper">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+
+        <Header />
+        <Outlet />
+        <Footer />
+    </>
+}
+
+export default Rootlayout
